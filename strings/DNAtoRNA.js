c95c7540a16c5abfb8eaad63b553272f9bb632b2
Adrian Hards <github@ahards.com>
2022-06-30 12:00:27 +0100

    Update to include link to problem

--- strings/DNAtoRNA.js
+++ strings/DNAtoRNA.js
@@ -1,3 +1,5 @@
+// link to problem: https://www.codewars.com/kata/5556282156230d0e5e000089
+
 function DNAtoRNA(dna) {
   // create a function which returns an RNA sequence from the given DNA sequence
   // split the string into an array by letter; map iterates and saves result;
