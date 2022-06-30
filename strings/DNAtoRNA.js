75c188a5825583d07dea1af57643c1115171afdb
Adrian Hards <github@ahards.com>
2022-06-30 12:00:58 +0100

    Modify README to clean notes

--- strings/DNAtoRNA.js
+++ strings/DNAtoRNA.js
@@ -1,7 +1,7 @@
 // link to problem: https://www.codewars.com/kata/5556282156230d0e5e000089
+// create a function which returns an RNA sequence from the given DNA sequence
 
 function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
   // split the string into an array by letter; map iterates and saves result;
   // for each element, if it matches T assign U else return original
   // join array and return a string with no commas
