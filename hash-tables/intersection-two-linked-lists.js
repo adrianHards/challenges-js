db0cfe73782c4f39732c6e60cc5cd561fb113465
Adrian Hards <github@ahards.com>
2022-07-05 14:25:48 +0100

    add notes to solution

--- hash-tables/intersection-two-linked-lists.js
+++ hash-tables/intersection-two-linked-lists.js
@@ -1,13 +1,16 @@
 function getIntersectionNode(headA, headB) {
-  // convert each array into an object (ES8+)
+  // convert each array into an object (below is ES8; use Object.assign if not supported)
   const objA = { ...headA }
   const objB = { ...headB }
 
-  //
-  for (const num in objA) {
-    if (objA[num] === objB[num]) {
-      return objA[num]
+  // loop over each key-value pair in first object
+  for (const key in objA) {
+    // if the value exists in the other object
+    if (objA[key] === objB[key]) {
+      // return the value
+      return objA[key]
     }
   }
+  // loop ended, meaning there is no link value so return null
   return null
 };
