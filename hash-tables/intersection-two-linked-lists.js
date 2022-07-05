3b54b55a6c1ed6552fb1052facb344a9c2728dd2
Adrian Hards <github@ahards.com>
2022-07-05 15:54:36 +0100

    added solution but invalid

--- hash-tables/intersection-two-linked-lists.js
+++ hash-tables/intersection-two-linked-lists.js
@@ -1,21 +1,35 @@
+// invalid solution
+
 function getIntersectionNode(headA, headB) {
   // convert each array into an object (below is ES8; use Object.assign if not supported)
   const objA = { ...headA }
   const objB = { ...headB }
+  let arrA = new Array()
+  let arrB = new Array()
 
-  // loop over each key-value pair in first object
+  // loop over each key-value pair in objA
   for (const key in objA) {
     // if the value exists in the other object
     if (Object.values(objB).includes(objA[key])) {
-      // return the value
-      return objA[key]
+      // save the shared value in an array
+      arrA.push(objA[key])
     }
   }
-  // loop ended, meaning there is no link value so return null
+
+  // do the same for objB
+  for (const key in objB) {
+    if (Object.values(objA).includes(objB[key])) {
+      arrB.push(objB[key])
+    }
+  }
+
+  // loop either array and return the first value that matches at index i
+  for (const i in arrA) {
+    if (arrA[i] === arrB[i]) {
+      return arrA[i]
+    }
+  }
+
+  // if there is no link value return null
   return null
 };
-
-// headA = [4, 1, 8, 4, 5]
-// headB = [5, 6, 1, 8, 4, 5]
-
-// console.log(getIntersectionNode(headA, headB))
